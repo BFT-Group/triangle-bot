f38991f04397cee7eb1651066131158cae9e672b
That one guy <96666783+CdcOnGitHub@users.noreply.github.com>
2023-05-11 17:55:50 +0200

    document 1

--- src/main.py
+++ src/main.py
@@ -1,15 +1,14 @@
-import os
+# Import all the necessary dependencies
 import disnake
 from disnake.ext import commands,tasks
-import time
 
+# Set-Up the bot
 intents = disnake.Intents.all()
 bot = commands.Bot(command_prefix='t!', intents=intents)
 
-
+# When the bot will be ready print into console
 @bot.event
 async def on_ready():
   print(f"Online on {bot.user}")
   
-#
-bot.run(os.environ['token'])
+#bot.run(os.environ['token'])

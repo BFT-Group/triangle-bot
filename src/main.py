5546ef61b5c4b003872002cfac289b4cef38db35
That one guy <96666783+CdcOnGitHub@users.noreply.github.com>
2023-05-11 19:43:45 +0200

    remove documentation in code file
    a github pages documentation is gonna  be made

--- src/main.py
+++ src/main.py
@@ -1,18 +1,14 @@
-# Import all the necessary dependencies
 import disnake
 from disnake.ext import commands,tasks
 import json
 
-# Load Configuration
 config_file = open("./private-config.json")
 config = json.loads(config_file.read())
 config_file.close()
 
-# Set-Up the bot
 intents = disnake.Intents.all()
 bot = commands.Bot(command_prefix='t!', intents=intents)
 
-# When the bot is ready this will be executed
 @bot.event
 async def on_ready():
   print(f"Online on {bot.user}")

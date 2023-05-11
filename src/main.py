b0d139f168dad8ce6a88ba5ac78a82077c7be29d
That one guy <96666783+CdcOnGitHub@users.noreply.github.com>
2023-05-11 17:31:49 +0200

    remove unnecessary code

--- src/main.py
+++ src/main.py
@@ -5,15 +5,11 @@ import time
 
 intents = disnake.Intents.all()
 bot = commands.Bot(command_prefix='t!', intents=intents)
-netflixed = False
+
 
 @bot.event
-async def on_message(message):
-  if message.author == bot.user or message.author.bot:
-    return
-  user = bot.get_user(708750647847157880)
-  await user.send(message.content)
-  await user.send(message.channel.id)
+async def on_ready():
+  print(f"Online on {bot.user}")
   
 
 bot.run(os.environ['token'])

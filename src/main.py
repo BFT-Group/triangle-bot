beae3b51e4ebaedec560da038060a58356c540c5
That one guy <96666783+CdcOnGitHub@users.noreply.github.com>
2023-05-12 14:12:02 +0200

    Fail Prevention

--- src/main.py
+++ src/main.py
@@ -8,10 +8,24 @@ config_file = open("./private-config.json")
 config = json.loads(config_file.read())
 config_file.close()
 
+class Triangle:
+  instance_type = "Local"
+  actions_hour_loops = 0
+
+triangle_bot = Triangle()
+
 intents = disnake.Intents.all()
 bot = commands.Bot(command_prefix='t!', intents=intents)
 
-
+@tasks.loop(hours=5,minutes=59)
+async def actions_restart_bot():
+  if triangle_bot.actions_hour_loops > 0:
+    info_channel = await bot.fetch_channel(1026074277432283186)
+    await info_channel.send("`🚨 OUTAGE ALERT 🚨` Step Runtime Limit Reached! Shutting Down.")
+    print("Step Limit Reached")
+    print("Shutting Down To Prevent Failure")
+    await bot.close()
+  triangle_bot.actions_hour_loops += 1
 
 @bot.slash_command()
 async def triangle(inter):
@@ -36,11 +50,15 @@ async def on_ready():
     if sys.argv[1] != None:
       commit = sys.argv[1][:7]
       summary = sys.argv[2]
-      commitstring = f" [{commit}] ({summary})"
+      if summary != "":
+        commitstring = f" [{commit}] ({summary})"
+      else:
+        commitstring = f" [{commit}]"
   except:
     print("Running Locally")
   else:
     print("Running On Actions")
+    triangle_bot.instance_type = "Actions"
   await bot.change_presence(activity=disnake.Game(name=f"{bot.command_prefix}help{commitstring}"))
   
 bot.run(config['token'])
